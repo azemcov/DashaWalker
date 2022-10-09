de3eb3fbb7db7b0a3e77c735bd1020c01a1962c8
azemcov <111581518+azemcov@users.noreply.github.com>
2022-10-09 18:00:01 +0500

    Update code.js

--- code.js
+++ code.js
@@ -5,15 +5,15 @@
     let xN = 0;
     let yN = 0;
 
-    document.getElementById('up').onclick = yUp;
-    document.getElementById('down').onclick = yDown;
-    document.getElementById('left').onclick = xLeft;
-    document.getElementById('right').onclick = xRight;
+//     document.getElementById('up').onclick = yUp;
+//     document.getElementById('down').onclick = yDown;
+//     document.getElementById('left').onclick = xLeft;
+//     document.getElementById('right').onclick = xRight;
 
-//     document.getElementById('up').addEventListener("click", yUp);
-//     document.getElementById('down').addEventListener("click", yDown);
-//     document.getElementById('left').addEventListener("click", xLeft);
-//     document.getElementById('right').addEventListener("click", xRight);
+    document.getElementById('up').addEventListener("click", yUp);
+    document.getElementById('down').addEventListener("click", yDown);
+    document.getElementById('left').addEventListener("click", xLeft);
+    document.getElementById('right').addEventListener("click", xRight);
 
     function yUp()
     {
